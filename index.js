0893379e03c43973cf21788c7e49ae945f25a7a5
dsl2022 <2015rpro@gmail.com>
2023-07-27 21:12:15 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const axios = require("axios")
 const BASEURL="https://pokeapi.co/api/v2"
+
+async function getPokes(URL){
+    try{
+       const result = await axios.get(`${URL}/pokemon`)
+       const {data} = result
+       console.log(data)
+    }catch(error){    
+    }
+}
+
+getPokes(BASEURL)
